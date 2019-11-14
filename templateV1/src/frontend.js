addbcb8b887abe1e18c873dfe536c54e149e1004
Student <example@example.com>
2019-11-14 11:16:56 -0800

    template

---
+++ templateV1/src/frontend.js
@@ -0,0 +1,22 @@
+import { pingPong } from './backend.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+//for ping pong
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+
+//For Ping Pong
