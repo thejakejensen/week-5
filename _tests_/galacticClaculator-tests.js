45c86e5685bdba1fad3dc9fee6b39326644788e5
Jacob Jensen <thejakejensen@gmail.com>
2019-11-15 16:04:59 -0800

    create a test in the tests folder but the tests folder is broken

--- _tests_/galacticClaculator-tests.js
+++ _tests_/galacticClaculator-tests.js
@@ -1,1 +1,8 @@
 import { Age } from './../src/backend.js';
+
+describe ('Age', function(){
+  it('should not accept non-numerical input', function() {
+    let exampleAge = new Age("peanut");
+    expect(exampleAge.checkValidity()).toBe(false);
+  });
+});
--- src/backend.js
+++ src/backend.js
@@ -2,4 +2,16 @@ export class Age {
   constructor(age) {
     this.age = age;
   }
+  checkValidity() {
+    let age;
+    let invalidLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    for(let i=0; i< invalidLetters.length; i++) {
+      if (this.age.toString().includes(invalidLetters[i])) {
+        return false;
+      } else {
+        age = true;
+      }
+    }
+    return age;
+  }
 };
