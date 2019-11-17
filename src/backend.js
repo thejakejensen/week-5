bb9c78741b3486f5ee473857459fbc83b1127414
Jacob Jensen <thejakejensen@gmail.com>
2019-11-16 22:16:09 -0800

    fix some syntax errors in js

--- src/backend.js
+++ src/backend.js
@@ -4,7 +4,7 @@ export class Age {
   }
   checkValidity() {
     let age;
-    let invalidLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    let invalidLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     for(let i=0; i< invalidLetters.length; i++) {
       if (this.age.toString().includes(invalidLetters[i])) {
         return false;
@@ -14,4 +14,4 @@ export class Age {
     }
     return age;
   }
-};
+}
